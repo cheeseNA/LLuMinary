a8d231f14fd6cb2d695337c6fa69243c119dd89a
cheeseNA <kurocat2000@gmail.com>
2023-10-20 20:45:23 +0200

    Add main.py

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    os.getenv("FRONTEND_URL", "http://localhost:3000"),
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
