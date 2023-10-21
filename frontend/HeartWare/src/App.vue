87f3aa79b4b4dfaf0abe4c353991a911040913b0
Yifei Han <han.yifei@outlook.com>
2023-10-21 13:53:57 +0200

    heyyy

--- frontend/HeartWare/src/App.vue
+++ frontend/HeartWare/src/App.vue
@@ -1,23 +1,12 @@
 <script setup lang="ts">
 import Handler from './components/Handler.vue';
+import MessageArea from './components/MessageArea.vue';
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<MessageArea />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView /> -->
-<Handler />
 </template>
 
 <style scoped>
