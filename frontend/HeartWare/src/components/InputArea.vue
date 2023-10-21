cf146e4aed2b2a6534699ef5725df5324b471dd6
Victor Willems <willems@student-net-hg-dock-1-c-0361.intern.ethz.ch>
2023-10-21 11:32:44 +0200

    Input Area Barebone

---
+++ frontend/HeartWare/src/components/InputArea.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+const input = ref("");
+const emit = defineEmits(["response"]);
+</script>
+
+<template>
+  <input v-model="input" type="text" />
+  <button v-on:click="emit('response', input)">Send</button>
+</template>
+
+<style></style>
