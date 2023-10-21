94f0fbcb5d3e21adf1416a116200eeec421336e6
Yifei Han <han.yifei@outlook.com>
2023-10-21 22:43:02 +0200

    fixed message area icons

--- frontend/HeartWare/src/main.ts
+++ frontend/HeartWare/src/main.ts
@@ -11,10 +11,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { faHippo } from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
-library.add(faUserSecret);
+library.add(faHippo);
 
 const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
 
