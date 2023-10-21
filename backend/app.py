6a3179f7f8b61b4710df48dda55a6a2597a71300
Hepaul7 <paulhe0773@gmail.com>
2023-10-21 09:46:50 +0200

    context bot

--- backend/app.py
+++ backend/app.py
@@ -23,25 +23,53 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def read_root(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
+
 
 @app.websocket("/chat")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        user_message = await websocket.receive_text()
-        print(user_message)
-        user_messages.append(user_message)
-        response = chatbot_response(user_message)
-        await websocket.send_text(response)
+        # user_message = await websocket.receive_text()
+        # print(user_message)
+        # user_messages.append(user_message)
+
+        # response = chatbot_response(user_message)
+
+        bot = Bot(200)
+        chat = True
+        with bot.model.chat_session():
+            # TODO initialize chat session
+            while chat:
+                # new_input = input('Type something > ')
+                user_message = await websocket.receive_text()
+                print(user_message)
+                user_messages.append(user_message)
+
+                if user_message == 'END':
+                    chat = False
+                    print(bot.get_chat_session())  # TODO store chat session
+                else:
+                    new_resp = bot.generate(user_message)
+                    print(new_resp)
+                    # print(bot.generate(new_input))
+                    await websocket.send_text(new_resp)
+
 
 def chatbot_response(user_input):
     bot = Bot(200)
     response = bot.generate(user_input)
     print(response)
     return response
+
+# def chatbot_response(user_input: str, bot: Bot):
+#     response = bot.generate(user_input)
+#     print(response)
+#     return response
+
 
 if __name__ == "__main__":
     import uvicorn
