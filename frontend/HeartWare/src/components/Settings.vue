e602ec2555aa2e5dcdd333d45c5e06999f2b4260
Victor Willems <willems@student-net-hg-dock-1-c-0428.intern.ethz.ch>
2023-10-21 21:51:07 +0200

    Added Settings

---
+++ frontend/HeartWare/src/components/Settings.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const extended = ref(false);
+</script>
+
+<template>
+  <div v-if="extended"></div>
+  <div v-else>
+    <font-awesome-icon :icon="['fas', 'gear']" />
+  </div>
+</template>
+
+<style></style>
