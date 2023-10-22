42d0f6575b0a216237c192e5e75b0f44786ea7e8
Victor Willems <willems@student-net-hg-dock-1-c-0428.intern.ethz.ch>
2023-10-22 03:13:46 +0200

    ff

---
+++ frontend/HeartWare/src/main.ts
@@ -0,0 +1,31 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faHippo,
+  faGear,
+  faTrash,
+  faMusic,
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faHippo);
+library.add(faGear);
+library.add(faTrash);
+library.add(faMusic);
+
+const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
+
+app.use(router);
+
+app.mount("#app");
